b262c6f1d4537004fd2b1efb9841d02d2179798e
tribh1 <tribh.ccgroup@gmail.com>
2020-02-01 16:38:58 +0700

    1 file modified

--- src/app/draw-use-case/draw-use-case.component.ts
+++ src/app/draw-use-case/draw-use-case.component.ts
@@ -15,19 +15,19 @@ export class DrawUseCaseComponent implements OnInit {
   ngOnInit() {
     this.ctx = this.canvas.nativeElement.getContext("2d");
     this.list = [
-      "Thêm mới trí hữu bùi tvhanh niên cứng việt nam 201032 a13 313",
-      "Sửa",
-      "Xóa",
-      "Xem chi tiết",
-      "In",
-      "Kết xuất Excel",
-      "Xem chi tiết",
-      "In",
-      "Kết xuất Excel"
+      "1.Thêm mới trí hữu bùi tvhanh niên cứng việt nam 201032 a13 313",
+      "2.Sửa",
+      "3.Xóa",
+      "4.Xem chi tiết",
+      "5.In",
+      "6.Kết xuất Excel",
+      "7.Xem chi tiết",
+      "8.In",
+      "9.Kết xuất Excel"
     ];
   }
   drawUseCase(): void {
-    let x = 600;
+    let x = 100;
     let y = 100;
     let restartIndex =Math.round(this.list.length / 2);
    
@@ -37,25 +37,22 @@ export class DrawUseCaseComponent implements OnInit {
       this.ctx.fillStyle = "back";
       //this.ctx.moveTo(0,this.list.length/2*120);
       let currentY = y + i * 120;
-      if (restartIndex > i) {
-        currentY = y + (i - restartIndex) * 120;
-      }
-      this.ctx.ellipse(x, currentY, 50, 75, Math.PI / 2, 0, 2 * Math.PI);
+      let currentX = x;
+       if (i>restartIndex) {
+        //this.ctx.translate(200,150);
+         //console.info(restartIndex);
+      }     
+      this.ctx.ellipse(currentX, currentY, 50, 75, Math.PI / 2, 0, 2 * Math.PI);
       this.ctx.stroke();
       //draw text
       let textUseCase = this.list[i];
       let listLines = this.getLines(this.ctx, textUseCase, this.MAX_WIDTH);
       let textLeng = 0;
       let textIndex = 0;
-      for (let j = 0; j < listLines.length; j++) {
-        //this.ctx.beginPath();
+      for (let j = 0; j < listLines.length; j++) {        
         this.ctx.font = "12px serif";
         this.ctx.fillText(listLines[j], x - 50, currentY - 20 + j * 20);
-      }
-      if (i>restartIndex) {
-        this.ctx.translate(-200, 0);
-         //console.info(restartIndex);
-      }
+      }     
      
     }
   }
